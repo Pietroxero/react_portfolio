67ed57b129169ed1e9a0c01d1a63b9cd9a76106a
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-04-19 15:33:38 -0700

    path correct to resume content

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Pietroxero/react_portfolio/src/Assets/solpeter.pdf";
+  "https://raw.githubusercontent.com/Pietroxero/react_portfolio/master/src/Assets/solpeter.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
